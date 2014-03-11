dd89acb763e5479b0bb82ed1b883c9a640e10925
Gra <golebier@gmail.com>
2014-03-11 13:37:29 +0100

    Began.

---
+++ src/main/java/jxbc/first/FirstInterface.java
@@ -0,0 +1,9 @@
+package jxbc.first;
+/**
+*
+* @author Gra <Gołębiewski Radosław A.> google.com/+RadoslawGolebiewski
+*
+*/
+public interface FirstInterface {
+
+}
---
+++ src/test/java/jxbc/first/FirstInterfaceTest.java
@@ -0,0 +1,20 @@
+package jxbc.first;
+
+import org.testng.annotations.Test;
+/**
+*
+* @author Gra <Gołębiewski Radosław A.> google.com/+RadoslawGolebiewski
+*
+*/
+public class FirstInterfaceTest {
+    @Test
+    public void f() {
+        // TODO IT
+        //      read xml bwmeta
+        //      convert to json
+        //      write/read
+        //      convert to bwmeta xml
+        //      write/read
+        //      read YElements and compare
+    }
+}
